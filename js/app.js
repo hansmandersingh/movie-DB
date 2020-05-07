98d4cd7e8dfbc6d3d3c62b8a40ec24b0e8f76e1b
Hans <hansmandersingh1998@gmail.com>
2020-05-07 11:05:45 -0500

    Some spacing and function changes

--- js/app.js
+++ js/app.js
@@ -2,41 +2,13 @@ const root = document.getElementById('root');
 
 function gettingTrendingMovies() {
   fetch(
-    "https://api.themoviedb.org/3/trending/all/week?api_key=7b403300d752c7d5e552aa9ff4e21a66"
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=7b403300d752c7d5e552aa9ff4e21a66"
   )
     .then((data) => {
       return data.json();
     })
     .then((data) => {
-      data.results.forEach((movie) => {
-        movie.genre_ids.forEach((genre) => {
-          let ele = root.querySelector(`.y${genre}`);
-          let titlesWrapper;
-
-          if (ele !== null) {
-            ele.classList.remove('hide');
-            titlesWrapper= ele.querySelector('.titles-wrapper');
-            titlesWrapper.insertAdjacentHTML('beforeend', `
-            <div class="movie">
-            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-            <div class="overlay">
-              <div class="title">${movie.title}</div>
-              <div class="rating">${movie.vote_average}/10</div>
-              <div class="plot">
-                ${movie.overview}
-              </div>
-              <div class="listToggle">
-                <div>
-                  <i class="fa fa-fw fa-plus"></i>
-                  <i class="fa fa-fw fa-check"></i>
-                </div>
-                </div>
-              </div>
-            </div>
-          `)
-          }
-        });
-      });
+      moviePrint(data.results);
     });
 }
 
@@ -54,9 +26,37 @@ function genreSearch() {
     })
 }
 
-genreSearch();
-gettingTrendingMovies();
+function moviePrint(dataResults) {
+  dataResults.forEach((movie) => {
+    movie.genre_ids.forEach((genre) => {
+      let ele = root.querySelector(`.y${genre}`);
+      let titlesWrapper;
 
+      if (ele !== null) {
+        ele.classList.remove('hide');
+        titlesWrapper= ele.querySelector('.titles-wrapper');
+        titlesWrapper.insertAdjacentHTML('beforeend', `
+        <div class="movie">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+        <div class="overlay">
+          <div class="title">${movie.title}</div>
+          <div class="rating">${movie.vote_average}/10</div>
+          <div class="plot">
+            ${movie.overview}
+          </div>
+          <div class="listToggle">
+            <div>
+              <i class="fa fa-fw fa-plus"></i>
+              <i class="fa fa-fw fa-check"></i>
+            </div>
+            </div>
+          </div>
+        </div>
+      `)
+      }
+    });
+  });
+}
 
 function insertGenres(genreName, genreId) {
   root.insertAdjacentHTML('beforeend', `
@@ -70,3 +70,8 @@ function insertGenres(genreName, genreId) {
     </div>
   `)
 }
+
+genreSearch();
+gettingTrendingMovies();
+
+
